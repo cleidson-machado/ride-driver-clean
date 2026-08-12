ca661dbd898132e32811bf003a7c6e10680ecfa1
Cleidson <cleidson.mac@outlook.com>
2026-08-12 11:13:16 +0100

    Fix: Update import path for TrashView and restore its implementation

--- lib/features/home_add_ride/home_content_tab_view.dart
+++ lib/features/home_add_ride/home_content_tab_view.dart
@@ -4,7 +4,7 @@ import '../financial_history/financial_history_view.dart';
 import '../history/presentation/history_view.dart';
 import '../search/presentation/search_view.dart';
 import '../tour_in_progress/tour_in_progress_view.dart';
-import '../trash/presentation/trash_view.dart';
+import '../trash/trash_view.dart';
 import 'home_add_ride_view.dart';
 
 /// Hub principal com abas compartilhando a mesma NavigationBar.
